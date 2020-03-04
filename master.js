22cd74b24cd65d0bd5cb4db4adcd2be5b9d328cd
Keviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2020-03-04 14:37:57 +0100

    fix

--- master.js
+++ master.js
@@ -7,13 +7,9 @@ const subscriptionName = 'segmenter-worker-djtxk38k4';
 const formattedSubscription = subClient.subscriptionPath(projectId, subscriptionName);
 
 const NB_RANGES = 95;
+const ACK_DEADLINE_SECONDS = 20;
 
-/*
- * PubSub pull method is not guaranteed
- * to return the exact number of messages of the maximum specified
- * @param pulledMessages {Array} Messages already pulled
- */
-const pullMessages = async (pulledMessages = []) => {
+const pullMessages = async (pulledMessages = [], countRetry) => {
   const request = {
     subscription: formattedSubscription,
     maxMessages: pulledMessages.length ? NB_RANGES - pulledMessages.length : NB_RANGES,
@@ -22,7 +18,9 @@ const pullMessages = async (pulledMessages = []) => {
   const [response] = await subClient.pull(request);
   const messages = [...pulledMessages, ...response.receivedMessages];
   if (messages.length > NB_RANGES) { return messages.slice(0, NB_RANGES); }
-  if (messages.length < NB_RANGES) { return pullMessages(messages); }
+  if (messages.length < NB_RANGES) {
+    if (countRetry < 15) return pullMessages(messages, countRetry + 1);
+  }
   return messages;
 };
 
@@ -30,26 +28,25 @@ const getData = async (messages) => messages.map(({ message: { data } }) => data
 
 const forkWorkers = async (datas) => Promise.all(datas.map((data) => cluster.fork({ data })));
 
-
 const acknowledge = async (ackIds) => subClient.acknowledge({
   subscription: formattedSubscription,
   ackIds,
 });
 
-const resetAckRequest = (ackIds) => subClient.modifyAckDeadline({
+const resetAckDeadline = (ackIds) => subClient.modifyAckDeadline({
   subscription: formattedSubscription,
   ackIds,
-  ackDeadlineSeconds: 20,
+  ackDeadlineSeconds: ACK_DEADLINE_SECONDS,
 });
 
 const handleChildExit = ({ id }, code, messages) => {
   if (code === 0) {
     messages.splice(id - 1, 1);
     if (!messages.length) { return process.exit(0); }
-  } else {
-    const data = messages[id - 1].message.data.toString();
-    throw new Error(`A worker failed his job: ${data}`);
+    return null;
   }
+  const data = messages[id - 1].message.data.toString();
+  throw new Error(`A worker failed his job: ${data}`);
 };
 
 const sleep = async (timeout) => new Promise((resolve) => setTimeout(resolve, 5000));
@@ -60,7 +57,7 @@ module.exports = async () => {
   await forkWorkers(data.slice(0, 4));
   cluster.on('exit', (worker, code) => handleChildExit(worker, code, messages));
   while (messages.length) {
-    resetAckRequest(messages.map(({ ackId }) => ackId));
+    resetAckDeadline(messages.map(({ ackId }) => ackId));
     // eslint-disable-next-line no-await-in-loop
     await sleep(10000);
   }
--- worker.js
+++ worker.js
@@ -1,3 +1,4 @@
 module.exports = async () => {
+  console.log("test");
   process.exit(0);
 };
